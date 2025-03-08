74ac9fa649fa6205c1e3bf6e4ccbdf8284896bd8
gnz-11x1 <sayakmajumder@hotmail.com>
2025-03-08 20:29:44 +0530

    math

---
+++ 004math/3exercises/2areaOfCircle.py
@@ -0,0 +1,7 @@
+import math 
+
+radius = float(input("Enter the radius of the circle: "))
+
+area = math.pi * pow(radius, 2)
+
+print(f"Area: {round(area, 2)} sq. unit")
---
+++ 004math/3exercises/3hypotenuseCalculator.py
@@ -0,0 +1,8 @@
+import math
+
+a = float(input("Enter the adjacent: "))
+b = float(input("Enter the opposite: "))
+
+c = math.sqrt(pow(a,2) + pow(b,2))
+
+print(f"The hypotenuse of the triangle is: {c} unit")
