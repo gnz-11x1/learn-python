75722181b5e54eb5934df84414fc1c58416e1c28
gnz-11x1 <sayakmajumder@hotmail.com>
2025-03-09 12:16:24 +0530

    mathCalculator

--- 006mathCalculator/main.py
+++ 006mathCalculator/main.py
@@ -1,4 +1,20 @@
 # Python calculator
 
 operator = input("Choose the operator (+,-,*,/): ")
+a = float(input("Choose the 1st number: "))
+b = float(input("Choose the 2nd number: "))
 
+if operator == "+":
+    c = a + b
+    print(f"The sum of a and b is {c}")
+elif operator == "-" :
+    c = a - b
+    print(f"The subtraction of a and b is {c}")
+elif operator == "*" :
+    c = a * b
+    print(f"The multiplication of a and b is {c}")
+elif operator == "/" :
+    c = a / b
+    print(f"The division between a and b is {c}")
+else :
+    print("Choose the operator between +, -, *, /")
