13d6f3a9bf63ac328b18f3758b09383ea8fa92ac
gnz-11x1 <sayakmajumder@hotmail.com>
2025-03-09 13:23:57 +0530

    weightConverter

--- 007weightConvertor/main.py
+++ 007weightConvertor/main.py
@@ -1,2 +1,18 @@
 # weight convertor
 
+unit = input("Kilograms or Pounds? (K or L): ")
+weight = float(input("Enter your weight: "))
+
+
+
+if unit == "K":
+    weight = weight * 2.205
+    unit = "Lbs."
+    print(f"You weight is : {round(weight, 1)} {unit}")
+elif unit == "L":
+    weight = weight / 2.205
+    unit = "Kgs."
+    print(f"You weight is : {round(weight, 1)} {unit}")
+else:
+    print(f"{unit} was not valid")
+
