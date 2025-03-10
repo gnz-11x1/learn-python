81e02e2e043a5c5efe968577625e43c02fc77d2b
gnz-11x1 <sayakmajumder@hotmail.com>
2025-03-10 08:50:15 +0530

    stringMethods

---
+++ 011stringMethods/main.py
@@ -0,0 +1,19 @@
+
+# name = input("Enter your full name: ")
+# phone_number = input("Enter your phone number #: ")
+
+# # result = len(name)
+# # result = name.find("S")
+# # result = name.rfind("q")
+# # name = name.capitalize()
+# # name = name.upper()
+# # name = name.lower()
+# # result = name.isdigit()
+# # result = name.isalpha()
+# # result = phone_number.count("9")
+# phone_number = phone_number.replace("9", "4")
+
+# print(phone_number)
+
+
+print(help(str))
